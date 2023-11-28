4ced3bcf741ac4d2ed62e24efe3017c9b95033e2
necodeus <dawid@necodeo.com>
2023-11-28 20:34:57 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,13 @@ export default defineNuxtConfig({
             noExternal: ["moment"],
         },
     },
-    runtimeConfig: { },
+    runtimeConfig: {
+        public: {
+            'COMMON_API_URL': process.env.COMMON_API_URL || 'http://common-api.localhost/api/v1',
+            'PAPER_API_URL': process.env.PAPER_API_URL || 'http://paper-api.localhost/api/v1',
+            'IMAGES_URL': process.env.IMAGES_URL || 'http://images.localhost',
+        }
+    },
     app: {
         head: {
             htmlAttrs: {
--- server/config/env.ts
+++ server/config/env.ts
@@ -1,8 +1,9 @@
-export const COMMON_API_URL = process.env.COMMON_API_URL || 'http://common-api.localhost/api/v1'
-export const PAPER_API_URL = process.env.PAPER_API_URL || 'http://paper-api.localhost/api/v1'
-export const IMAGES_URL = process.env.IMAGES_URL || 'http://images.localhost'
+export const COMMON_API_URL = process.env.NUXT_PUBLIC_COMMON_API_URL || 'http://common-api.localhost/api/v1'
+export const PAPER_API_URL = process.env.NUXT_PUBLIC_PAPER_API_URL || 'http://paper-api.localhost/api/v1'
+export const IMAGES_URL = process.env.NUXT_PUBLIC_IMAGES_URL || 'http://images.localhost'
 
-console.log('NODE_ENV', process.env)
-console.log('COMMON_API_URL', COMMON_API_URL)
-console.log('PAPER_API_URL', PAPER_API_URL)
-console.log('IMAGES_URL', IMAGES_URL)
+console.log('API', {
+    COMMON_API_URL,
+    PAPER_API_URL,
+    IMAGES_URL,
+})
