564564ccec24aed509edf42a1114bbbb1589d4a0
necodeus <dawid@necodeo.com>
2023-12-17 21:33:50 +0100

    chore(0): test

--- components/Back.vue
+++ components/Back.vue
@@ -2,7 +2,7 @@
     <div class="transition-colors hover:scale-[1.02] bg-transparent hover:bg-white border-[#000] hover:border-[#fff] text-[#000] hover:text-[#000]">
         <NuxtLink
             to="/"
-            class="m-[2px] scale-1 tracking-[4px] text-[11px] font-bold inline-block py-[15px] px-[20px] border-2 border-solid uppercase"
+            class="m-[2px] scale-1 tracking-[4px] text-[11px] font-bold inline-block py-[15px] px-[20px] border-2 border-solid border-[#000] uppercase"
         >Wstecz</NuxtLink>
     </div>
 </template>
