fb10621a6f442ba349b240c6df168ee8895b9b60
necodeus <dawid@necodeo.com>
2024-01-04 18:21:46 +0100

    fix(0): fix shell scripts

--- start-development.sh
+++ start-development.sh
@@ -1,1 +1,10 @@
-NUXT_PUBLIC_API_BASE_URL=http://admin-api.localhost/api/v1 npm run dev
+export NUXT_PUBLIC_COMMON_API_URL=https://common-api.necodeo.com/api/v1
+export NUXT_PUBLIC_PAPER_API_URL=https://paper-api.necodeo.com/api/v1
+export NUXT_PUBLIC_FORUM_API_URL=https://forum-api.necodeo.com/api/v1
+export NUXT_PUBLIC_ADMIN_API_URL=https://admin-api.necodeo.com/api/v1
+export NUXT_PUBLIC_SHOP_API_URL=https://shop-api.necodeo.com/api/v1
+export NUXT_PUBLIC_IMAGES_URL=https://images.necodeo.com
+export NUXT_PUBLIC_WEBSOCKET_SESSION_HOST=necodeo.com
+export NUXT_PUBLIC_WEBSOCKET_ADDRESS=wss://www.necodeo.com:8090
+
+yarn dev
