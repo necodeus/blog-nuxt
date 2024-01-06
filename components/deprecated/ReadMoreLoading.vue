a1c1365e20b9061d4c906b10fbdb5dcc00b2560c
necodeus <dawid@necodeo.com>
2024-01-06 21:24:17 +0100

    refactor(0): rename deprecated files

--- components/deprecated/ReadMoreLoading.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div
-        class="text-[11px] tracking-[4px] font-bold bg-[white] uppercase mx-0 px-5 py-[15px] border-2 border-solid border-black animated-background"
-    >
-        Czytaj więcej
-    </div>
-</template>
