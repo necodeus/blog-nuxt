3a852c4e96bef257bf6c4cfa70b49c6677c9304f
necodeus <dawid@necodeo.com>
2024-01-06 12:18:16 +0100

    refactor(0): refactor

--- server/api/posts/[postId]/comments/[commentId]/downvote.post.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const { postId, commentId } = event.context.params as any
-
-    return await downvoteCommentById(postId, commentId).catch(() => null)
-})
--- server/api/posts/[postId]/comments/index.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const { postId } = event.context.params as any
-
-    return await getCommentsByPostId(postId).catch(() => null)
-})
