9f3bd0e7f97eb946702ac29c10f77c2c1cd0b4a2
Dawid Smulewicz <dawid@necodeo.com>
2024-02-18 16:32:47 +0100

    test

--- middleware/page.ts
+++ middleware/page.ts
@@ -1,13 +1,13 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const nuxtApp = useNuxtApp()
 
-    if (process.client) {
-        const { data, pending } = useFetch<any>(`/api/_page?path=${to.path}`)
+    if (process.server) {
+        const { data, pending } = await useFetch<any>(`/api/_page?path=${to.path}`)
 
         nuxtApp.$requestData = data
         nuxtApp.$requestDataPending = pending
     } else {
-        const { data, pending } = await useFetch<any>(`/api/_page?path=${to.path}`)
+        const { data, pending } = useFetch<any>(`/api/_page?path=${to.path}`)
 
         nuxtApp.$requestData = data
         nuxtApp.$requestDataPending = pending
